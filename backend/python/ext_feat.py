ff1916be8b7a71d47179bc8728c26fd6c4944ec7
Sree Harsha <142214147+charapakasaisreeharsha@users.noreply.github.com>
2025-04-27 21:04:02 +0530

    Update ext_feat.py

--- backend/python/ext_feat.py
+++ backend/python/ext_feat.py
@@ -1,3 +1,6 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Suppress TensorFlow logging
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from tensorflow.keras.applications import ResNet50
